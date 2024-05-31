1107ee30226640e88a48eda729c7846ba803b4d1
B1129009 <120005700+B1129009@users.noreply.github.com>
2024-06-01 03:30:56 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,21 +14,6 @@ const db = mysql.createConnection({
 });
 
 // Snake endpoints
-app.post("/createSnake", (req, res) => {
-  const { snakeID, name, poison, time, color, pattern, headShape, antivenomId } = req.body;
-  db.query(
-    "INSERT INTO 蛇的種類 (Snake_ID, 種類, 毒性, 出沒時間, 顏色, 斑紋, 頭部形狀, Antivenom_ID) VALUES (?,?,?,?,?,?,?,?)",
-    [snakeID, name, poison, time, color, pattern, headShape, antivenomId],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send(err);
-      } else {
-        res.send("Values Inserted");
-      }
-    }
-  );
-});
 
 app.get("/snakes", (req, res) => {
   db.query("SELECT * FROM 蛇的種類", (err, result) => {
@@ -40,6 +25,112 @@ app.get("/snakes", (req, res) => {
   });
 });
 
+
+// Hospital endpoints
+
+app.get("/hospitals", (req, res) => {
+  db.query("SELECT hospital.醫事機構代碼, hospital.醫院名稱, 醫院電話, 醫院地址, 藥品名稱 FROM hospital left join 存放位置 on hospital.醫事機構代碼 = 存放位置.醫事機構代碼 and hospital.醫院名稱 = 存放位置.醫院名稱;", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+
+app.get("/poisonLevels", (req, res) => {
+  db.query("SELECT * FROM 蛇的毒性", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get("/snakeColors", (req, res) => {
+  db.query("SELECT * FROM 蛇的顏色", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get("/snakePatterns", (req, res) => {
+  db.query("SELECT * FROM `蛇的斑紋`", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get("/head", (req, res) => {
+  db.query("SELECT * FROM `蛇的頭部形狀`", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+// Add this to your existing server code
+app.get("/cities", (req, res) => {
+  db.query("SELECT city_id, city FROM city", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.get("/sites", (req, res) => {
+  const city = req.query.city;
+  db.query(
+    "SELECT site_id, site FROM site JOIN city ON site.city_id = city.city_id WHERE city.city = ?",
+    [city],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.get("/roads", (req, res) => {
+  const site = req.query.site;
+  db.query(
+    "SELECT road_id, road FROM road JOIN site ON road.site_id = site.site_id WHERE site.site = ?",
+    [site],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
 app.put("/updateSnake", (req, res) => {
   const { id, field, value } = req.body;
   db.query(
