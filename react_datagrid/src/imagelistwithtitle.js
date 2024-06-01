e61ee6b63355992526d55b2b8eb8fecb86e1920e
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-06-01 17:14:06 +0800

    Update imagelistwithtitle.js

--- react_datagrid/src/imagelistwithtitle.js
+++ react_datagrid/src/imagelistwithtitle.js
@@ -153,7 +153,7 @@ const ImageListWithTitle = () => {
             </Button>
             <ImageListItemBar
               title={item.種類}
-              subtitle={<span>{item.毒性} {item.藥品名稱}</span>}
+              subtitle={<span>「{item.毒性}」 「{item.藥品名稱}」</span>}
               position="below"
             />
           </ImageListItem>
