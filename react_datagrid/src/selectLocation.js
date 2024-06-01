68554aef49279c8ea5d562f4b01b87b5318eb3db
B1129009 <120005700+B1129009@users.noreply.github.com>
2024-06-01 23:04:22 +0800

    Update selectLocation.js

--- react_datagrid/src/selectLocation.js
+++ react_datagrid/src/selectLocation.js
@@ -5,6 +5,7 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const SelectLocation = ({ handleLocationChange }) => {
   const [location, setLocation] = useState({ city: '', district: '', road: '', serum: '' });
@@ -12,35 +13,28 @@ const SelectLocation = ({ handleLocationChange }) => {
   const [districts, setDistricts] = useState([]);
   const [roads, setRoads] = useState([]);
   const [serums, setSerums] = useState([]);
+  const [loadingCities, setLoadingCities] = useState(true);
+  const [loadingDistricts, setLoadingDistricts] = useState(false);
+  const [loadingRoads, setLoadingRoads] = useState(false);
 
   useEffect(() => {
-    Axios.get("http://localhost:3001/cities")
-      .then((response) => {
-        setCities(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching city data:', error);
-      });
+    Axios.get("http://192.168.137.196:3001/cities")
+      .then(response => setCities(response.data))
+      .catch(error => console.error('Error fetching city data:', error))
+      .finally(() => setLoadingCities(false));
 
-    Axios.get("http://localhost:3001/snakeSerum")
-      .then((response) => {
-        console.log('Serum data:', response.data); // Debugging line
-        setSerums(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching snakeSerum data:', error);
-      });
+    Axios.get("http://192.168.137.196:3001/snakeSerum")
+      .then(response => setSerums(response.data))
+      .catch(error => console.error('Error fetching snakeSerum data:', error));
   }, []);
 
   useEffect(() => {
     if (location.city) {
-      Axios.get(`http://localhost:3001/sites?city=${location.city}`)
-        .then((response) => {
-          setDistricts(response.data);
-        })
-        .catch((error) => {
-          console.error('Error fetching district data:', error);
-        });
+      setLoadingDistricts(true);
+      Axios.get(`http://192.168.137.196:3001/sites?city=${location.city}`)
+        .then(response => setDistricts(response.data))
+        .catch(error => console.error('Error fetching district data:', error))
+        .finally(() => setLoadingDistricts(false));
     } else {
       setDistricts([]);
       setRoads([]);
@@ -49,22 +43,19 @@ const SelectLocation = ({ handleLocationChange }) => {
 
   useEffect(() => {
     if (location.district) {
-      Axios.get(`http://localhost:3001/roads?site=${location.district}`)
-        .then((response) => {
-          setRoads(response.data);
-        })
-        .catch((error) => {
-          console.error('Error fetching road data:', error);
-        });
+      setLoadingRoads(true);
+      Axios.get(`http://192.168.137.196:3001/roads?site=${location.district}`)
+        .then(response => setRoads(response.data))
+        .catch(error => console.error('Error fetching road data:', error))
+        .finally(() => setLoadingRoads(false));
     } else {
       setRoads([]);
     }
   }, [location.district]);
 
-
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setLocation((prevState) => {
+    setLocation(prevState => {
       const newLocation = { ...prevState, [name]: value };
       if (name === 'city') {
         newLocation.district = '';
@@ -77,10 +68,18 @@ const SelectLocation = ({ handleLocationChange }) => {
     });
   };
 
+  const menuProps = {
+    PaperProps: {
+      style: {
+        width: 250, // Set the width of the dropdown menu
+      },
+    },
+  };
+
   return (
     <div className="containerStyle">
       <div>
-        <h1 className="styleArgument">選擇被咬的位置</h1>
+        <h1 className="styleArgument">選擇所在位置</h1>
       </div>
       <FormControl variant="outlined" sx={{ minWidth: 120, marginBottom: 2 }}>
         <InputLabel id="city-select-label">城市</InputLabel>
@@ -90,14 +89,15 @@ const SelectLocation = ({ handleLocationChange }) => {
           value={location.city}
           onChange={handleChange}
           label="城市"
+          MenuProps={menuProps}
         >
           <MenuItem value=""><em>None</em></MenuItem>
-          {cities.map((city) => (
+          {loadingCities ? <MenuItem disabled><CircularProgress size={24} /></MenuItem> : cities.map(city => (
             <MenuItem key={city.city_id} value={city.city}>{city.city}</MenuItem>
           ))}
         </Select>
       </FormControl>
-      <FormControl variant="outlined" sx={{ minWidth: 120, marginBottom: 2 }}>
+      <FormControl variant="outlined" sx={{ minWidth: 120, marginBottom: 2 }} disabled={!location.city}>
         <InputLabel id="district-select-label">區域</InputLabel>
         <Select
           labelId="district-select-label"
@@ -105,15 +105,15 @@ const SelectLocation = ({ handleLocationChange }) => {
           value={location.district}
           onChange={handleChange}
           label="區域"
-          disabled={!location.city}
+          MenuProps={menuProps}
         >
           <MenuItem value=""><em>None</em></MenuItem>
-          {districts.map((district) => (
+          {loadingDistricts ? <MenuItem disabled><CircularProgress size={24} /></MenuItem> : districts.map(district => (
             <MenuItem key={district.site_id} value={district.site}>{district.site}</MenuItem>
           ))}
         </Select>
       </FormControl>
-      <FormControl variant="outlined" sx={{ minWidth: 120 }}>
+      <FormControl variant="outlined" sx={{ minWidth: 120, marginBottom: 2 }} disabled={!location.district}>
         <InputLabel id="road-select-label">道路</InputLabel>
         <Select
           labelId="road-select-label"
@@ -121,10 +121,10 @@ const SelectLocation = ({ handleLocationChange }) => {
           value={location.road}
           onChange={handleChange}
           label="道路"
-          disabled={!location.district}
+          MenuProps={menuProps}
         >
           <MenuItem value=""><em>None</em></MenuItem>
-          {roads.map((road) => (
+          {loadingRoads ? <MenuItem disabled><CircularProgress size={24} /></MenuItem> : roads.map(road => (
             <MenuItem key={road.road_id} value={road.road}>{road.road}</MenuItem>
           ))}
         </Select>
@@ -137,9 +137,10 @@ const SelectLocation = ({ handleLocationChange }) => {
           value={location.serum}
           onChange={handleChange}
           label="藥品名稱"
+          MenuProps={menuProps}
         >
           <MenuItem value=""><em>None</em></MenuItem>
-          {serums.map((serum) => (
+          {serums.map(serum => (
             <MenuItem key={serum.serum_id} value={serum.藥品名稱}>{serum.藥品名稱}</MenuItem>
           ))}
         </Select>
