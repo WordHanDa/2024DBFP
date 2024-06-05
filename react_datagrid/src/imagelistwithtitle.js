fee1383e8877a4c3895ea632c7999a3e1501d230
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2024-06-05 14:19:13 +0800

    p

--- react_datagrid/src/imagelistwithtitle.js
+++ react_datagrid/src/imagelistwithtitle.js
@@ -10,7 +10,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import Avatar from '@mui/material/Avatar';
 
-const SERVER_ADDRESS = "http://172.27.6.192:3001";
+const SERVER_ADDRESS = "http://172.20.10.4:3001";
 
 const imageListContainerStyle = {
   display: 'flex',
--- react_datagrid/src/selectLocation.js
+++ react_datagrid/src/selectLocation.js
@@ -7,7 +7,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import CircularProgress from '@mui/material/CircularProgress';
 
-const SERVER_ADDRESS = 'http://172.27.6.192:3001';
+const SERVER_ADDRESS = 'http://172.20.10.4:3001';
 
 const SelectLocation = ({ handleLocationChange }) => {
   const [location, setLocation] = useState({ city: '', district: '', road: '', serum: '' });
