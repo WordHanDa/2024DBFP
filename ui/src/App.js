ebd8c6454438d8ed3d71f98957dc1be7d08fd44a
B1129009 <120005700+B1129009@users.noreply.github.com>
2024-06-05 15:59:32 +0800

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,26 @@
-import'./App.css'
+import './App.css';
 import SeurmMapTitle from './ptitle';
 import SerumMapFooter from './pfooter';
 import SerumMapHeader from './pheader';
 import SnakeFeature from './snakefeature';
 import ImageListWithTitle from './imagelistwithtitle';
-import ParentComponent from './parentcomponent';
+import SelectLocation from './selectLocation';
+import Datagrid from './datagrid';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [selectedLocation, setSelectedLocation] = useState({ city: '', district: '', road: '', serum: '' });
+  const [selectedSerum, setSelectedSerum] = useState('');
+
+  const handleLocationChange = (location) => {
+    setSelectedLocation(location);
+  };
+
+  const handleImageClick = (serum) => {
+    setSelectedSerum(serum);
+    setSelectedLocation(prevLocation => ({ ...prevLocation, serum }));
+  };
+
   return (
     <div>
       <div>
@@ -19,16 +33,19 @@ function App() {
         <SnakeFeature />
       </div>
       <div className="containerStyle">
-        <ImageListWithTitle />
+        <ImageListWithTitle onImageClick={handleImageClick} />
       </div>
       <div>
-        <ParentComponent/>
+        <SelectLocation selectedSerum={selectedSerum} handleLocationChange={handleLocationChange} />
+      </div>
+      <div>
+        <Datagrid selectedLocation={selectedLocation} />
       </div>
       <div>
         <SerumMapFooter />
       </div>
     </div>
   );
-}
+};
 
 export default App;
